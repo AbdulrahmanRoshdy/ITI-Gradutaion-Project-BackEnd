f1772deb0cdc1fe515c1c728f72d258bf2c7ebd7
AbdulrahmanRoshdy <AbdulrahmanRoshdy@outlook.com>
2021-03-22 18:49:02 +0200

    Updated: Uploaded photos and mongodb environment variables

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+const dotenv = require("dotenv");
+dotenv.config()
+
 
 class Connection {
   constructor() {
     const url =
       process.env.MONGODB_URI ||
-      `mongodb+srv://Abdulrahman:80EWOa7uLLr3iBoJ@upworkgraduationproject.bsr20.mongodb.net/Upwork?retryWrites=true&w=majority`;
+      `mongodb+srv:///${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@FreelancerZone.bsr20.mongodb.net/Upwork?retryWrites=true&w=majority`;
     console.log("Establish new connection with url", url);
     mongoose.Promise = global.Promise;
     mongoose.set("useNewUrlParser", true);
